758b850f9fc2c43e7eb2c22dad17656c2339bc24
Bryce Verberne <bryceverberne@gmail.com>
2026-09-01 20:51:44 -0600

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    int getLargest(const unordered_map<char, int>& catalog) {
+        int largest = 0;
+        
+        for (const auto& [key, value] : catalog) {
+            largest = max(largest, value);
+        }
+
+        return largest;
+    }
+
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> catalog;
+        int count = 0;
+        int maxf = 0;
+        int L = 0;
+
+        for (int R = 0; R < s.size(); ++R) {
+            int window = R - L + 1;
+
+            // Add encountered character
+            ++catalog[s[R]];
+            maxf = max(maxf, catalog[s[R]]);
+
+            // Ensure the window size is valid
+            while (window - maxf > k) {
+                --catalog[s[L]];
+                --window;
+                ++L;
+            }
+
+            count = max(count, window);
+        }
+
+        return count;
+    }
+};
